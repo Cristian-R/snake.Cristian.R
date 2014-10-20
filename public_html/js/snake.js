c87c0220c397550ef681a422255078620ed2252b
Cristian Robles <cristianrobles177@gmail.com>
2014-10-20 14:37:20 -0700

    snake can move

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,11 +1,16 @@
 var snake;
-             
+var snakeLength;
+var snakeSize;
+
+var food;
+
 var context;
 var screenWidth;
 var screenHeight;
 
 gameInitialize();
-gameDraw();
+snakeInitialize();
+setInterval(gameLoop, 1000/30);
 
 function gameInitialize(){
   var canvas = document.getElementById('game-screen');
@@ -17,10 +22,52 @@ function gameInitialize(){
   canvas.width = screenWidth;
   canvas.height = screenHeight; 
 }
+ 
 function gameLoop(){
-    
+  gameDraw();
+  snakeUpdate();  
+  snakeDraw(); 
 }
 function gameDraw(){
   context.fillStyle = 'rgb(57, 101,145 )';
   context.fillRect(0, 0, screenWidth, screenHeight);
+}
+function snakeInitialize(){
+    snake = [];
+    snakeLength = 5;
+    snakeSize = 30;
+    
+    for(var index = snakeLength - 1; index >= 0; index--){
+       snake.push( {
+           x: index,
+           y: 0
+       });
+     }
+ }
+ 
+function snakeDraw(){
+    for(var index = 0; index < snake.length; index++){
+        context.fillStyle = 'white';
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    } 
+}
+
+function snakeUpdate(){
+   var snakeHeadX = snake[0].x;
+   var snakeHeadY = snake[0].y;
+   
+   snakeHeadX++;
+   
+   var snakeTail = snake.pop();
+   snakeTail.x = snakeHeadX;
+   snakeTail.y = snakeHeadY;
+   snake.unshift(snakeTail);
+}
+
+function foodInitialize(){
+    
+}
+
+function foodDraw(){
+    
 }
